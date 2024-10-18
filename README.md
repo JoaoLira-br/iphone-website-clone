94144f33e34d44aeec2a732c47f6975e761fa865
Joao Victor Lira <lira440440@gmail.com>
2024-10-18 10:24:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# A clone of the Apple Iphone website 
+
+This clone was inspired by this video https://www.youtube.com/watch?v=RbxHZwFtRT4&list=PL6QREj8te1P6wX9m5KnicnDVEucbOPsqR from 
+JSMastery echo "# iphone-website-clone" >> README.md
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { appleImg } from "../utils";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt="apple image" />
+      </nav>
+      Navbar
+    </header>
+  );
+};
+
+export default Navbar;
